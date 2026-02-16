99272dee4603f6897e9b7e5c129aecce9c3f71a8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-16 14:10:02 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,11 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=./AppDir/bin/resources/build/icon.png
+export DESKTOP=https://gitlab.archlinux.org/archlinux/packaging/packages/element.io/-/raw/main/io.element.Element.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
