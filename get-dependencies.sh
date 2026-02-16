d6aba3c6a0b4ff7856ec8735734366b0ddf1db22
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-16 14:41:56 -0400

    fix weird issue

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -24,7 +24,13 @@ esac
 TARBALL=https://packages.element.io/desktop/install/linux/glibc-"$tarball_arch"/element-desktop.tar.gz
 wget "$TARBALL" -O /tmp/tarball.tar.gz
 tar xfv /tmp/tarball.tar.gz
-echo ./element-desktop-* | awk -F'-' '{print $NF; exit}' > ~/version
+
+# for x86_64 upstream names the directory element-desktop-1.12.10
+# while for aarch64 the directory is named element-desktop-1.12.10-arm64
+case "$ARCH" in
+	x86_64)  echo ./element-desktop-* | awk -F'-' '{print $NF; exit}'     > ~/version;;
+	aarch64) echo ./element-desktop-* | awk -F'-' '{print $(NF-1); exit}' > ~/version;;
+esac
 
 mkdir -p ./AppDir/bin
 mv -v ./element-desktop-*/* ./AppDir/bin
