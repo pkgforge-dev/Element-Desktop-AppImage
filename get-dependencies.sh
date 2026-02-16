1070d492bc0c5faa3a4242fb6d347f85bde7ffb3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-16 14:02:51 -0400

    package app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,12 +15,16 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
 
-# If the application needs to be manually built that has to be done down here
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+# upstream uses x86-64 instead of x86_64
+case "$ARCH" in
+	x86_64) tarball_arch=x86-64;;
+	aarch64) tarball_arch=$ARCH;;
+esac
+TARBALL=https://packages.element.io/desktop/install/linux/glibc-"$tarball_arch"/element-desktop.tar.gz
+wget "$TARBALL" -O /tmp/tarball.tar.gz
+tar xfv /tmp/tarball.tar.gz
+echo ./element-desktop-* | awk -F'-' '{print $NF; exit}' ~/version
+
+mkdir -p ./AppDir/bin
+mv -v ./element-desktop-*/* ./AppDir/bin
